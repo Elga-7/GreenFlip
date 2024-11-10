53d6e7590c6a36c010031cf9922fe39532df7a53
Ana D <anadprofessional@gmail.com>
2024-11-10 15:52:31 -0400

    CSS landing page

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function toggleNavbar() {
     const navbarLinks = document.querySelector('.navbar-links');
     navbarLinks.classList.toggle('active');
 }
+
+function toggleTexto(parrafoId) {
+    const parrafo = document.getElementById(parrafoId);
+    const boton = parrafo.nextElementSibling; // El botón está después del párrafo
+    
+    if (parrafo.classList.contains('oculto')) {
+        parrafo.classList.remove('oculto');
+        parrafo.classList.add('mostrar-todo');
+        boton.textContent = 'Leer menos';
+    } else {
+        parrafo.classList.remove('mostrar-todo');
+        parrafo.classList.add('oculto');
+        boton.textContent = 'Leer más';
+    }
+}
